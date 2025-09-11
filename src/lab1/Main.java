0a7a469c9dbd7fa48e41ef6f503a51a84beca832
Dekre0109 <156953620+Dekre0109@users.noreply.github.com>
2025-09-11 17:04:11 -0400

    Add Main.java for Task 3

---
+++ src/lab1/Main.java
@@ -0,0 +1,9 @@
+package lab1;
+
+public class Main {
+     static void main(String[] args) {
+        // Call the helper method from FizzBuzz
+        FizzBuzz.doFizzBuzz(5);
+        FizzBuzz.doFizzBuzz(7);
+    }
+}
